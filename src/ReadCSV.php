19022ce719b7d2480824f317f2c86e96300400aa
hbwsl <sumitshitole1994@gmail.com>
2017-08-04 10:33:27 +0530

    added logic of merge csv

--- src/ReadCSV.php
+++ src/ReadCSV.php
@@ -1,45 +1,45 @@
-<?php 
+<?php
+
 namespace Migration;
+
 use League\Csv\Reader;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ReadCSV extends Command
-{
-    protected function configure()
-    {
+class ReadCSV extends Command {
+
+    protected function configure() {
         $this
-        // the name of the command (the part after "bin/console")
-        ->setName('app:create-user')
+                // the name of the command (the part after "bin/console")
+                ->setName('app:readcsv')
 
-        // the short description shown while running "php bin/console list"
-        ->setDescription('Creates a new user.')
+                // the short description shown while running "php bin/console list"
+                ->setDescription('Read given CSV.')
 
-        // the full command description shown when running the command with
-        // the "--help" option
-        ->setHelp('This command allows you to create a user...')
-    ;
+                // the full command description shown when running the command with
+                // the "--help" option
+                ->setHelp('This command allows you to read given csv and return array...')
+        ;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
+    protected function execute(InputInterface $input, OutputInterface $output) {
         // ...
-		$output->writeln([
-        '<info>User Creator</info>',
-        '<error>============</error>',
-        '',
-    ]);
-		if(file_exists('/home/hbwsl/MigrationTry/1.csv'))
-		{
-			$csv = Reader::createFromPath('/home/hbwsl/MigrationTry/1.csv');
+        $output->writeln([
+            '<info>User Creator</info>',
+            '<error>============</error>',
+            '',
+        ]);
+        if (file_exists('/home/hbwsl/MigrationTry/1.csv')) {
+            $csv = Reader::createFromPath('/home/hbwsl/MigrationTry/1.csv');
 
-			//get the first row, usually the CSV header
-			$results = $csv->fetchAssoc();
-			foreach ($results as $row) {
-			//echo $row;
-				print_r($row);
-			}	
-		}
+            //get the first row, usually the CSV header
+            $results = $csv->fetchAssoc();
+            foreach ($results as $row) {
+                //echo $row;
+                print_r($row);
+            }
+        }
     }
+
 }
--- src/index.php
+++ src/index.php
@@ -5,5 +5,6 @@ require __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\Console\Application;
 $application = new Application();
 $application->add(new ReadCSV());
+$application->add(new CSVJoin());
 $application->run();
 // ... register commands
